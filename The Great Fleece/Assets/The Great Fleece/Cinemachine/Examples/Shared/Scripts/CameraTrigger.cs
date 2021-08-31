9b3b5a7910a00797c3e8d3ca3f86f1136c23fd56
Brett <babsportsfan@gmail.com>
2021-08-31 14:05:40 -0500

    Laptop from trip push

---
+++ The Great Fleece/Assets/The Great Fleece/Cinemachine/Examples/Shared/Scripts/CameraTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public Transform myCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Trigger Activated");
+            Camera.main.transform.position = myCamera.position;
+            Camera.main.transform.rotation = myCamera.rotation;
+        }
+    }
+
+}
+
---
+++ The Great Fleece/Assets/The Great Fleece/Cinemachine/Examples/Shared/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    NavMeshAgent _agent;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, out hit))
+            {
+                Debug.Log(hit.point);
+                _agent.SetDestination(hit.point);
+            }
+        }
+       
+
+    }
+}
